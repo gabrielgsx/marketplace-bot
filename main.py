8cb1a1ecb33c0ca6ef581706431176ca757cda6a
Gabriel Salvatore <62920284+gabrielgsx@users.noreply.github.com>
2021-11-07 18:02:08 -0300

    refactoring

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import pyautogui
 import os
-from ads.Ad import titulo, preco, descricao, cidades
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from xpathss import title, price, category1, category2, condition1, condition2, description, color, tags, tags1, place, scroll_box
+from ads.Ad import titulo, preco, descricao1, descricao2, descricao3, descricao4, descricao5, descricao6,  descricao7,  descricao8, descricao9, descricao10, cidades
+from xpathss import title, price, category1, category2, condition1, condition2, description, color, tags, tags1, place, finish1, finish2, scroll_box
+
 pyautogui.FAILSAFE = True
 
 options = webdriver.ChromeOptions()
 options.add_argument("user-data-dir=C:\\Users\\gabri\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 16")
 
 browser = webdriver.Chrome('chromedriver.exe', chrome_options=options)
+wait = WebDriverWait(browser, 10)
 
 # For tabbing, arrow keys, and interacting with a single webpage
 shortSleep = 0.2
@@ -19,6 +24,20 @@ shortSleep = 0.2
 # For all other loading, including file uploads, page refreshes or reloads, etc.
 longSleep = 5
 
+
+def postDescription():
+    sleep(2)
+    input_box = wait.until(EC.presence_of_element_located((By.XPATH, description)))
+    input_box.send_keys(f'{descricao1}' + Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao2}' + Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao3}' + Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao4}'+ Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao5}' + Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao6}' + Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao7}' + Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao8}'+ Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao9}'+ Keys.SHIFT + Keys.ENTER + Keys.ENTER)
+    input_box.send_keys(f'{descricao10}'+ Keys.SHIFT + Keys.ENTER + Keys.ENTER)
 
 def postAllImages():
     sleep(2)
@@ -28,6 +47,16 @@ def postAllImages():
         pyautogui.scroll(+100)
     images = getAdImagePaths()
     postImages(images[0])
+
+def postImages(images):
+    pyautogui.moveTo(150, 400)
+    sleep(shortSleep)
+    pyautogui.click()
+    sleep(5)
+    pyautogui.typewrite(images)
+    sleep(shortSleep)
+    pyautogui.press("enter")
+
 
 def getAdImagePaths():
     ads = os.getcwd() + "/ads/"
@@ -44,73 +73,64 @@ def getAdImagePaths():
             images.append(valores)
     return images
 
-def postImages(images):
-    pyautogui.moveTo(150, 400)
-    sleep(shortSleep)
-    pyautogui.click()
-    sleep(5)
-    pyautogui.typewrite(images)
-    sleep(shortSleep)
-    pyautogui.press("enter")
-
-
 def postAd():
-    # for city in cidades:
-    sleep(10)
-    browser.get('https://www.facebook.com/marketplace/create/item')
-    sleep(5)
+    for city in cidades:
+        sleep(10)
+        browser.get('https://www.facebook.com/marketplace/create/item')
+        sleep(5)
 
-    #LOAD THE ELEMENTS
-    sleep(2)
-    pyautogui.moveTo(150, 400)
-    sleep(1)
-    for c in range(30):
-        pyautogui.scroll(-100)
+        #LOAD THE ELEMENTS
+        sleep(2)
+        pyautogui.moveTo(150, 400)
+        sleep(1)
+        for c in range(30):
+            pyautogui.scroll(-100)
 
-    sleep(2)
-    #TITLE
-    browser.find_element_by_xpath(f'{title}').send_keys(f'{titulo}')
+        sleep(2)
+        #TITLE
+        browser.find_element_by_xpath(f'{title}').send_keys(f'{titulo}')
 
-    #PRICE
-    browser.find_element_by_xpath(f'{price}').send_keys(f'{preco}')
+        #PRICE
+        browser.find_element_by_xpath(f'{price}').send_keys(f'{preco}')
 
-    #CATEGORY
-    sleep(5)
-    browser.find_element_by_xpath(f'{category1}').click()
+        #CATEGORY
+        sleep(5)
+        browser.find_element_by_xpath(f'{category1}').click()
 
-    sleep(3)
-    browser.find_element_by_xpath(f'{category2}').click()
+        sleep(3)
+        browser.find_element_by_xpath(f'{category2}').click()
 
-    #CONDITION
-    sleep(5)
-    browser.find_element_by_xpath(f'{condition1}').click()
+        #CONDITION
+        sleep(5)
+        browser.find_element_by_xpath(f'{condition1}').click()
 
-    sleep(5)
-    browser.find_element_by_xpath(f'{condition2}').click()
+        sleep(5)
+        browser.find_element_by_xpath(f'{condition2}').click()
 
-    #DESCRIPTION
-    sleep(0.5)
-    browser.find_element_by_xpath(f'{description}').send_keys(f'{descricao}')
+        #DESCRIPTION
+        postDescription()
 
-    #TAGS
-    sleep(1)
-    browser.find_element_by_xpath(f'{tags}').send_keys(tags1)
-    sleep(0.3)
+        #TAGS
+        sleep(1)
+        browser.find_element_by_xpath(f'{tags}').send_keys(tags1)
+        sleep(0.3)
 
-    #PLACE
-    browser.find_element_by_xpath(f'{place}').send_keys(Keys.CONTROL, 'a')
-    browser.find_element_by_xpath(f'{place}').send_keys(Keys.DELETE)
-    browser.find_element_by_xpath(f'{place}').send_keys('Manaus')   
-    sleep(2)
-    browser.find_element_by_xpath(f"//span[contains(text(),'Manaus')]").click()
+        #PLACE
+        browser.find_element_by_xpath(f'{place}').send_keys(Keys.CONTROL, 'a')
+        browser.find_element_by_xpath(f'{place}').send_keys(Keys.DELETE)
+        browser.find_element_by_xpath(f'{place}').send_keys(f'{city}')   
+        sleep(2)
+        browser.find_element_by_xpath(f"//span[contains(text(),'{city}')]").click()
 
-    postAllImages()
+        postAllImages()
 
-    #FINISH
-    sleep(1)
-    browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[5]/div/div/div/div/div[1]/div/span').click()
-    sleep(5)
-    browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[5]/div[2]/div/div/div[1]/div/span').click()
+        #FINISH
+        sleep(1)
+        browser.find_element_by_xpath(f'{finish1}').click()
+        sleep(5)
+        browser.find_element_by_xpath(f'{finish2}').click()
+
+
 def main():
     postAd()
 
